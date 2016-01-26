e996c9311f9c22d8d9747ae05504cff0651d17b0
ripjyr <ripjyr@users.noreply.github.com>
2016-01-26 14:33:12 +0900

    Update ATEM-ON-OFF-Recordings.ino
    Not using attachInterrupt.

--- ATEM-ON-OFF-Recordings.ino
+++ ATEM-ON-OFF-Recordings.ino
@@ -1,14 +1,6 @@
 /* ——————————————
   ATEM On/Off Recording Button
   ——————————————— */
-// Off recording LED
-#define PMD4_OUTPUT() (DDRD |= _BV(4))    // pinMode(4,OUTPUT)
-#define DWD4_HIGH()   (PORTD |= _BV(4))   // digitalWrite(4,HIGH)
-#define DWD4_LOW()    (PORTD &= ~_BV(4))  // digitalWrite(4,LOW)
-// On Recording LED
-#define PMD5_OUTPUT() (DDRD |= _BV(5))    // pinMode(5,OUTPUT)
-#define DWD5_HIGH()   (PORTD |= _BV(5))   // digitalWrite(5,HIGH)
-#define DWD5_LOW()    (PORTD &= ~_BV(5))  // digitalWrite(5,LOW)
 
 #include <Ethernet.h>
 #include <SPI.h>
@@ -18,7 +10,16 @@
 ATEMstd AtemSwitcher;
 
 #include <MemoryFree.h>
+
+// DEBUG Switch.
 #define DEBUG // Debug Setting
+//#define MEM_DEBUG // Debug Setting
+
+int onLED  = 5;
+int offLED = 4;
+int button = 2;
+
+volatile bool SW_status = false;
 
 // Initialize the Ethernet client library
 EthernetClient client;
@@ -36,13 +37,14 @@ void setup() {
   // Start the Ethernet, Serial (debugging) and UDP:
   //  Ethernet.begin(mac, clientIp);
   Serial.begin(115200);
-  Serial.println(F("\nSerial Start"));
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.println(F("Serial Start"));
 
   // DHCPにてArduinoのIPアドレス取得
   if (Ethernet.begin(mac) == 0) {
-#ifdef DEBUG
-    Serial.println(F("\nDHCP Failed"));
-#endif
+    Serial.println(F("DHCP Failed"));
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, clientIp);
   }
@@ -51,15 +53,17 @@ void setup() {
   printIPAddress();
 #endif
 
+  // Off recording LED
+  pinMode(onLED, OUTPUT);
+  // On Recording LED
+  pinMode(offLED, OUTPUT);
+  // Button
+  pinMode(2, INPUT_PULLUP);
+
   // Initialize a connection to the switcher:
   AtemSwitcher.begin(switcherIp);
   AtemSwitcher.serialOutput(0x80);
   AtemSwitcher.connect();
-
-  //  attachInterrupt(0, offRecording, RISING  ); // GPIO 2nd pin
-  //  attachInterrupt(0, onRecording , FALLING ); // GPIO 2nd pin
-  attachInterrupt(0, offRecording, HIGH ); // GPIO 2nd pin
-  attachInterrupt(0, onRecording , LOW  ); // GPIO 2nd pin
 }
 
 void loop() {
@@ -67,49 +71,66 @@ void loop() {
   // VERY important that this function is called all the time - otherwise connection might be lost because packets from the switcher is
   // overlooked and not responded to.
   while (!AtemSwitcher.hasInitialized())  {
-    DWD5_LOW(); // digitalWrite(5,LOW)
-    DWD4_LOW(); // digitalWrite(4,LOW)
+    digitalWrite(onLED, LOW);
+    digitalWrite(offLED, LOW);
     AtemSwitcher.runLoop();
   }
-  if (AtemSwitcher.getDownstreamKeyerStatus(2) == true )  { // System OFF Recording
-    DWD4_LOW(); // digitalWrite(4,LOW)
-    DWD5_HIGH(); // digitalWrite(5,HIGH)
+
+  if (digitalRead(button) == HIGH) {
+    offRecording();
   } else {
-    DWD4_HIGH(); // digitalWrite(4,HIGH)
-    DWD5_LOW(); // digitalWrite(5,LOW)
+    onRecording();
   }
-#ifdef DEBUG
-  Serial.print(F("\nfreeMem="));
+
+#ifdef MEM_DEBUG
+  Serial.print(F("freeMem="));
   Serial.println(freeMemory());
-  delay(1000);
+  delay(200);
 #endif
 }
 
 void offRecording() {
 #ifdef DEBUG
-  Serial.println(F("\nOffRecord"));
+  Serial.println(F("OffRecord"));
 #endif
-  AtemSwitcher.changeAudioMasterVolume(33); // Master Volume to -60
-  AtemSwitcher.changeDownstreamKeyOn(2, true); // Downstream Keyer 2 OFF
+  if (!SW_status) {
+    AtemSwitcher.changeAudioMasterVolume(33); // Master Volume to -60
+    AtemSwitcher.changeDownstreamKeyOn(2, true); // Downstream Keyer 2 OFF
+    SW_status = true;
+    checkLED();
+  }
 }
+
 void onRecording() {
 #ifdef DEBUG
-  Serial.println(F("\nOnRecord"));
+  Serial.println(F("OnRecord"));
 #endif
-  AtemSwitcher.changeAudioMasterVolume(32789.13); // Master Volume to 0
-  AtemSwitcher.changeDownstreamKeyOn(2, false); // Downstream Keyer 2 ON
+  if (SW_status) {
+    AtemSwitcher.changeAudioMasterVolume(32789.13); // Master Volume to 0
+    AtemSwitcher.changeDownstreamKeyOn(2, false); // Downstream Keyer 2 ON
+    SW_status = false;
+    checkLED();
+  }
 }
-
-
 
 void printIPAddress()
 {
 #ifdef DEBUG
-  Serial.print(F("\nClient IP:"));
+  Serial.print(F("Client IP:"));
   for (byte thisByte = 0; thisByte < 4; thisByte++) {
     // print the value of each byte of the IP address:
     Serial.print(Ethernet.localIP()[thisByte], DEC);
     Serial.print(F("."));
   }
 #endif
+}
+
+void checkLED() {
+  if (AtemSwitcher.getDownstreamKeyerStatus(2) == true )  { // System OFF Recording
+    digitalWrite(onLED, LOW);
+    digitalWrite(offLED, HIGH);
+  } else {
+    digitalWrite(onLED, HIGH);
+    digitalWrite(offLED, LOW);
+  }
 }
